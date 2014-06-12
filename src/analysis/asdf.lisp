2f9cbab1dfcf216a078893358898137c6c5ee5ed
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-06-12 18:04:43 +0200

    Backport: Improved ASDF parsing in src/analysis/asdf.lisp
    * src/analysis/asdf.lisp (header): added one-line summary
      (with-reset-source-registry): removed; no longer needed
      (with-silenced-output): likewise
      (load-system): likewise
      (analyze pathname eql :asdf/one-file): use
      `%extract-system-definition-forms' instead of actually loading the
      system definition
      (%extract-system-definition-forms): new function; analyze system with
      less side effects

--- src/analysis/asdf.lisp
+++ src/analysis/asdf.lisp
@@ -1,49 +1,15 @@
-;;;; asdf.lisp ---
+;;;; asdf.lisp --- Analysis of ASDF projects.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2014 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:jenkins.analysis)
 
-(defmacro with-reset-source-registry ((directory) &body body)
-  "TODO(jmoringe): document"
-  (once-only (directory)
-    `(progn
-       (asdf:initialize-source-registry `(:source-registry
-                                          :ignore-inherited-configuration
-                                          (:tree ,,directory)))
-       ,@body)))
-
-(defmacro with-silenced-output (&body body)
-  `(let* ((*standard-output* (make-broadcast-stream))
-          (*error-output*    *standard-output*)
-          (*trace-output*    *standard-output*)
-          (*debug-io*        *standard-output*))
-     ,@body))
-
-(defun load-system (file)
-  (let ((name      (pathname-name file))
-        (directory (make-pathname :directory (pathname-directory file))))
-    (handler-case
-        (with-reset-source-registry (directory)
-          (handler-bind ((warning #'muffle-warning))
-            (progn #+no with-silenced-output
-                   (asdf:clear-system name)
-                   (let ((*load-truename* file))
-                     (asdf::load-sysdef name file))
-                   (asdf:find-system name))))
-      #+sbcl (sb-c:compiler-error (condition)
-               (error "~@<Could not load ~S: ~A~@:>"
-                      file (ignore-errors (princ-to-string condition)))))))
-
 (defmethod analyze ((file pathname)
                     (kind (eql :asdf/one-file))
                     &key)
-  (let+ ((system (load-system file))
-         (name (asdf:component-name system))
-         (version (asdf:component-version system))
-         ((&labels dependency-name (dependency)
+  (let+ (((&labels dependency-name (dependency)
             (etypecase dependency
               (cons (dependency-name (second dependency)))
               (t    (string-downcase dependency)))))
@@ -52,29 +18,32 @@
               ((cons (eql :version))
                (list :asdf (string-downcase (second dependency))
                      (parse-version (third dependency))))
-              (t
+              ((or string symbol)
                (list :asdf (string-downcase dependency))))))
-         (all-dependencies
-          (remove-duplicates
-           (asdf:component-sideway-dependencies system)
-           :test #'string-equal :key #'dependency-name))
-         (non-system-dependencies
-          (remove-if (compose
-                      (rcurry #'find (ql:system-list) :test #'string-equal :key #'ql-dist:name)
-                      #'dependency-name)
-                     all-dependencies)))
-    (append
-     (list :versions `((:main . ,version))
-           :provides `((:asdf ,name ,(parse-version version)))
-           :requires (mapcar #'dependency->list non-system-dependencies))
-     (when (asdf:system-description system)
-       `(:description ,(asdf:system-description system)))
-     (when (asdf:system-author system)
-       `(:authors (,(asdf:system-author system))))
-     (when (asdf:system-maintainer system)
-       `(:maintainers (,(asdf:system-maintainer system))))
-     (when (asdf:system-license system)
-       `(:license ,(asdf:system-license system))))))
+         (system-systems (ql:system-list))
+         ((&flet+ process-system-form ((&ign name
+                                        &key
+                                        version
+                                        license
+                                        author
+                                        maintainer
+                                        description
+                                        depends-on
+                                        &allow-other-keys))
+            (append
+             (list :versions `((:main . ,version))
+                   :provides `((:asdf ,(string-downcase name) ,(parse-version version)))
+                   :requires (mapcar #'dependency->list
+                                     (remove-if (compose
+                                                 (rcurry #'find system-systems
+                                                         :test #'string-equal :key #'ql-dist:name)
+                                                 #'dependency-name)
+                                                depends-on)))
+             (when description `(:description ,description))
+             (when author      `(:authors (,author)))
+             (when maintainer  `(:maintainers (,maintainer)))
+             (when license     `(:license ,license))))))
+    (mapcar #'process-system-form (%extract-system-definition-forms file))))
 
 (defmethod analyze ((directory pathname)
                     (kind      (eql :asdf))
@@ -89,3 +58,39 @@
                              (format stream "~@<Skip file ~S.~@:>"
                                      file))
                    (declare (ignore condition)))))))
+
+;;; Utility functions
+
+(defun %extract-system-definition-forms (pathname)
+  (let+ ((result   '())
+         (packages '())
+         (next-macroexpand-hook *macroexpand-hook*)
+         ((&flet next (function form env)
+            (when next-macroexpand-hook
+              (funcall next-macroexpand-hook function form env))))
+         ((&flet on-macro-expansion (function form env)
+            (typecase form
+              ;; Record definitions of not currently existing packages
+              ;; for later deletion in a cleanup step.
+              ((cons (eql defpackage)) ; TODO not portable, i guess
+               (let ((name (second form)))
+                 (unless (find-package name)
+                   (push name packages))
+                 (next function form env)))
+              ((cons (eql asdf:defsystem))
+               (push form result)
+               nil)
+              ((cons (eql defmethod))
+               nil)
+              (t
+               (next function form env)))))
+         (*macroexpand-hook* #'on-macro-expansion))
+    (unwind-protect
+         (progn
+           (handler-bind ((warning #'muffle-warning)) ; TODO silence everything?
+             (asdf::load-asd pathname))
+           (nreverse result))
+      (mapc (lambda (package)
+              (log:debug "~@<Deleting package ~S~@:>" package)
+              (ignore-errors (delete-package package)))
+            packages))))

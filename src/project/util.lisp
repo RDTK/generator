fe19a039a6be371f3ff55619c5ce5e2c86e40b09
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-10-20 15:30:03 +0200

    Hack to make ordering errors continuable in src/project/util.lisp
    * src/project/util.lisp (header): updated copyright
      (sort-with-partial-order): made failed ordering error continuable

--- src/project/util.lisp
+++ src/project/util.lisp
@@ -1,6 +1,6 @@
 ;;;; util.lisp --- Utilities used in the project module.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2014 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -16,10 +16,13 @@
                           (rotatef (nth i list) (nth j list))
                           (return-from outer t))))
           (return-from sort-with-partial-order list)))
-  (error "~<~S does not define a partial order on ~S.~@:>~%Problems:~%~{* ~{~A - ~A~}~^~%~}"
-         (list predicate list)
-         (iter outer
-               (for i :below (length list))
-               (iter (for j :from (1+ i) :below (length list))
-                     (when (funcall predicate (nth j list) (nth i list))
-                       (in outer (collect (list (nth i list) (nth j list)))))))))
+  (cerror "Continue without a valid order"
+          "~<~S does not define a partial order on ~S.~@:>~%~
+           Problems:~%~{* ~{~A - ~A~}~^~%~}"
+          (list predicate list)
+          (iter outer
+                (for i :below (length list))
+                (iter (for j :from (1+ i) :below (length list))
+                      (when (funcall predicate (nth j list) (nth i list))
+                        (in outer (collect (list (nth i list) (nth j list))))))))
+  list)

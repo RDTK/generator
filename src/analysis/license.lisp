1dbbb76d6eaa624a4b0ee9bf96b2dd0fd75f9286
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-10-27 19:20:20 +0100

    Use safe external format for license files in src/analysis/license.lisp
    * src/analysis/license.lisp (header): updated copyright
      (analyze pathname eql :license): use `read-file-into-string*' instead
      of `read-file-into-string' to ensure use of a safe external format

--- src/analysis/license.lisp
+++ src/analysis/license.lisp
@@ -1,6 +1,6 @@
 ;;;; license.lisp --- Analysis of license files.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2014 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -22,15 +22,15 @@
                                 (find-files
                                  (merge-pathnames "**/LICENSE.*" directory)))))
                 (system-file
-                 (let ((content  (read-file-into-string project-file))
+                 (let ((content  (read-file-into-string* project-file))
                        (licenses (directory "/usr/share/common-licenses/**/*.*")))
                    (or
                     ;; Fast path: exact match.
                     (find content licenses
                           :test #'string=
-                          :key  #'read-file-into-string)
+                          :key  #'read-file-into-string*)
                     ;; Slow path: edit distance.
                     (find content licenses
                           :test (lambda (x y) (< (edit-distance x y :upper-bound threshold) threshold))
-                          :key #'read-file-into-string)))))
+                          :key #'read-file-into-string*)))))
       (pathname-name system-file))))

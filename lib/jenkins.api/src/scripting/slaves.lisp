456f6099fea332903d1549c5d7a127ccd4fcaf60
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-01-06 13:23:07 +0100

    Removed old stuff in lib/jenkins.api/

--- lib/jenkins.api/src/scripting/slaves.lisp
+++
@@ -1,5 +0,0 @@
-(dolist (job (all-jobs))
-  (when (member "oneiric" (slaves job) :test #'ppcre:scan)
-    (setf (slaves job) (remove-if (curry #'ppcre:scan "oneiric") (slaves job)))
-    (format t "~64A ~{~A~^, ~}~%" job (slaves job))
-    (commit! job)))

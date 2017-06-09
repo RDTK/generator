8954324cedc5021921b54602a08e4cf4cbd93c69
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-06-09 13:39:17 +0200

    Moved tests to test directory in {src,test}/model/variables/grammar.lisp
    * src/model/variables/grammar.lisp (header): updated copyright
      (toplevel): removed test code
    * test/model/variables/grammar.lisp: new file

--- src/model/variables/grammar.lisp
+++ src/model/variables/grammar.lisp
@@ -1,6 +1,6 @@
 ;;;; grammar.lisp --- Grammar for value expressions.
 ;;;;
-;;;; Copyright (C) 2012, 2013, 2014, 2015, 2016 Jan Moringen
+;;;; Copyright (C) 2012-2017 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -66,27 +66,3 @@
 
 (esrap:defrule expr
     (* (or variable-reference text)))
-
-(mapc (lambda+ ((input expected))
-        (assert (equal (esrap:parse 'expr input) expected)))
-      '((""           nil)
-        ("foo"        ("foo"))
-        ("foo$bar"    ("foo$bar"))
-        ("foo{bar"    ("foo{bar"))
-        ("foo}bar"    ("foo}bar"))
-
-        ("${a}"       ((:ref ("a"))))
-        ("${a|}"      ((:ref ("a") :default nil)))
-        ("${a|b}"     ((:ref ("a") :default "b")))
-        ("${a|{b}}"   ((:ref ("a") :default "{b") "}"))
-        ("${a|${b}}"  ((:ref ("a") :default (:ref ("b")))))
-        ("${${a}}"    ((:ref ((:ref ("a"))))))
-        ("@{a}"       ((:ref/list ("a"))))
-        ("@{a|}"      ((:ref/list ("a") :default nil)))
-        ("@{a|b}"     ((:ref/list ("a") :default "b")))
-        ("@{a|{b}}"   ((:ref/list ("a") :default "{b") "}"))
-        ("@{a|${b}}"  ((:ref/list ("a") :default (:ref ("b")))))
-        ("@{a|a${b}}" ((:ref/list ("a") :default ("a" (:ref ("b"))))))
-        ("@{@{a}}"    ((:ref/list ((:ref/list ("a"))))))
-
-        ("foo${a}"    ("foo" (:ref ("a"))))))

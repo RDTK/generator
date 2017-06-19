93e6bb5d606469b78440f170b30b8f6ab7271eb8
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2017-06-19 16:34:35 +0200

    Added missing header in src/model/project/util.lisp
    * src/model/project/util.lisp (header): added; was missing

--- src/model/project/util.lisp
+++ src/model/project/util.lisp
@@ -1,3 +1,9 @@
+;;;; util.lisp --- Utilities used in the model.project module.
+;;;;
+;;;; Copyright (C) 2017 Jan Moringen
+;;;;
+;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+
 (cl:in-package #:jenkins.model.project)
 
 (defun+ parse-dependency-spec ((nature name &optional version))

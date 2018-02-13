2dc36ecc7e7b61e550f3c583619fc3cb6b903685
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-02-13 10:26:04 +0100

    Minor refactoring in src/analysis/archive.lisp
    * src/analysis/archive.lisp (header): updated copyright
      (download-file): extracted from `analyze' methodism
      (analyze uri eql :archive): factored download into separate function

--- src/analysis/archive.lisp
+++ src/analysis/archive.lisp
@@ -1,10 +1,27 @@
 ;;;; archive.lisp --- Access project that are distributed as archives.
 ;;;;
-;;;; Copyright (C) 2014, 2015, 2017 Jan Moringen
+;;;; Copyright (C) 2014, 2015, 2017, 2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:jenkins.analysis)
+
+(defun download-file (url output-file &key username password)
+  (let+ (((&values input code &ign &ign &ign close-stream? reason)
+          (apply #'drakma:http-request url
+                 :want-stream  t
+                 :force-binary t
+                 :verify       nil
+                 (when (and username password)
+                   (list :basic-authorization (list username password))))))
+    (unless (<= 200 code 299)
+      (error "~@<Download from ~A failed with code ~D: ~A.~@:>"
+             url code reason))
+    (unwind-protect
+         (with-output-to-file (output output-file :element-type '(unsigned-byte 8))
+           (copy-stream input output))
+      (when close-stream?
+        (close input)))))
 
 (defmethod analyze ((source puri:uri) (kind (eql :archive))
                     &rest args &key
@@ -25,21 +42,9 @@
                          version.~@:>"
                         version)
             (with-trivial-progress (:download "~A" source)
-              (let+ (((&values input code &ign &ign &ign close-stream? reason)
-                      (apply #'drakma:http-request source
-                             :want-stream  t
-                             :force-binary t
-                             :verify       nil
-                             (when (and username password)
-                               (list :basic-authorization (list username password))))))
-                (unless (<= 200 code 299)
-                  (error "~@<Download from ~A failed with code ~D: ~A.~@:>"
-                         source code reason))
-                (unwind-protect
-                     (with-output-to-file (output temp-file :element-type '(unsigned-byte 8))
-                       (copy-stream input output))
-                  (when close-stream?
-                    (close input)))))
+              (download-file source temp-file
+                             :username username
+                             :password password))
 
             (with-trivial-progress (:extract "~A" temp-file)
               (inferior-shell:run/nil `("unp" "-U" ,temp-file) :directory temp-directory)

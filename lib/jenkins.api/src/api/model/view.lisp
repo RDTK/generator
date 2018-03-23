afd90f08ccc26400cc9efae244d6f143580626ee
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-03-23 13:43:18 +0100

    Backport: Added properties, filter, columns in lib/jenkins.api/src/api/model/view.lisp
    * lib/jenkins.api/src/api/model/view.lisp (header): updated copyright
      (*default-columns*): new parameter; default columns for newly
      created views
      (type string/column): new type; for conversion of columns
      (xml-> element eql string/column): new method; column element to string
      (->xml string element eql string/column): new method; string to
      column element
      (view::properties): new slot; for view properties
      (view::job-filters): new slot; for view job filters
      (view::columns): new slot; for view columns
    
    (cherry picked from commit e20ef57589242b43e3038307a5e22cd03f86afc8)

--- lib/jenkins.api/src/api/model/view.lisp
+++ lib/jenkins.api/src/api/model/view.lisp
@@ -1,21 +1,63 @@
 ;;;; view.lisp --- View model class.
 ;;;;
-;;;; Copyright (C) 2012-2017 Jan Moringen
+;;;; Copyright (C) 2012-2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:jenkins.api)
 
+(defparameter *default-columns*
+  '("hudson.views.StatusColumn"
+    "hudson.views.WeatherColumn"
+    "hudson.views.BuildButtonColumn"
+    "hudson.views.JobColumn"
+    "hudson.views.LastSuccessColumn"
+    "hudson.views.LastFailureColumn"
+    "hudson.views.LastDurationColumn"))
+
+(deftype string/column ()
+  'string)
+
+(defmethod xloc:xml-> ((value stp:element)
+                       (type  (eql 'string/column))
+                       &key inner-types)
+  (declare (ignore inner-types))
+  (stp:local-name value))
+
+(defmethod xloc:->xml ((value string)
+                       (dest  stp:element)
+                       (type  (eql 'string/column))
+                       &key inner-types)
+  (declare (ignore inner-types))
+  (setf (stp:local-name dest) value))
+
 (define-model-class view ()
-    ((id          :type     string
-                  :xpath    "name/text()"
+    ((id          :type      string
+                  :xpath     "name/text()"
                   :optional? nil)
-     (description :type     (or null string)
-                  :xpath    "description/text()"
-                  :initform nil)
-     (jobs        :type     (singleton-element "text()")
-                  :xpath    ("jobNames[comparator[@class=\"hudson.util.CaseInsensitiveComparator\"]]/string"
-                             :if-multiple-matches :all)))
+     (description :type      (or null string)
+                  :xpath     "description/text()"
+                  :initform  nil)
+     (properties  :type      (singleton-element "text()")
+                  :xpath     ("properties[@class=\"hudson.model.View$PropertyList\"]/property"
+                              :if-multiple-matches :all)
+                  :initform  '()
+                  :optional? nil)
+     (jobs        :type      (singleton-element "text()")
+                  :xpath     ("jobNames[comparator[@class=\"hudson.util.CaseInsensitiveComparator\"]]/string"
+                              :if-multiple-matches :all)
+                  :initform  '()
+                  :optional? nil)
+     (job-filters :type      (singleton-element "text()")
+                  :xpath     ("jobFilters/filter"
+                              :if-multiple-matches :all)
+                  :initform  '()
+                  :optional? nil)
+     (columns     :type      string/column
+                  :xpath     ("columns/*"
+                              :if-multiple-matches :all)
+                  :initform  *default-columns*
+                  :optional? nil))
   (:default-initargs
    :data (stp:make-document (stp:make-element "hudson.model.ListView")))
   (:get-func (lambda (id)      (view-config id)))

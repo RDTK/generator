6a35b2957a1a8aea66204fdf0b97082494f1bd1e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-05-17 14:15:40 +0200

    Added changelog support in src/commands/command-version.lisp
    * src/commands/command-version.lisp (header): updated copyright
      (version::changelog-count): new slot; number of changelog entries to
      print
      (*command-schema*): added changelog option
      (command-execute version): print changelog entries if requested

--- src/commands/command-version.lisp
+++ src/commands/command-version.lisp
@@ -1,13 +1,19 @@
 ;;;; command-version.lisp --- Command for printing relevant versions.
 ;;;;
-;;;; Copyright (C) 2017 Jan Moringen
+;;;; Copyright (C) 2017, 2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:jenkins.project.commands)
 
 (defclass version ()
-  ()
+  ((changelog-count :initarg  :changelog-count
+                    :type     (or boolean positive-integer)
+                    :reader   changelog-count
+                    :initform nil
+                    :documentation
+                    #.(format nil "Number of releases for which a list ~
+                       of changes should be printed.")))
   (:documentation
    "Print the version of this program and some components."))
 
@@ -15,16 +21,23 @@
  'command :version :class 'version)
 
 (jenkins.project.commandline-options:define-option-mapping
-    (*command-schema* "version"))
+    (*command-schema* "version")
+  (("-c" "--changelog") "changelog-count" "COUNT"))
 
 (defmethod command-execute ((command version))
-  (let* ((version   (generator-version))
+  (let* ((stream    *standard-output*)
+         (version   (generator-version))
          (versions  `(("build-generator"           ,version)
                       ("asdf"                      ,(asdf:asdf-version))
                       (,(lisp-implementation-type) ,(lisp-implementation-version))))
          (max-width (reduce #'max versions
                             :initial-value 0
                             :key           (compose #'length #'first))))
-    (format *standard-output* "~{~{~V:A ~:[~{~D.~D~^.~D~^-~A~}~;~A~]~}~&~}"
+    (format stream "~{~{~V:A ~:[~{~D.~D~^.~D~^-~A~}~;~A~]~}~&~}"
             (loop :for (name version) :in versions
-               :collect `(,max-width ,name ,(stringp version) ,version)))))
+                  :collect `(,max-width ,name ,(stringp version) ,version)))
+
+    (when-let ((count (changelog-count command)))
+      (let ((count (if (eq count t) nil count)))
+        (format stream "~2%")
+        (print-changelog (changelog :count count) :stream stream)))))

194b60561cb4cc3b5224392f13cc4dc5ab270483
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-05-22 14:23:14 +0200

    Improved documentation strings in src/commands/command-version.lisp
    * src/commands/command-version.lisp (version::changelog-count):
      improved documentation string
      (version): likewise

--- src/commands/command-version.lisp
+++ src/commands/command-version.lisp
@@ -13,9 +13,17 @@
                     :initform nil
                     :documentation
                     #.(format nil "Number of releases for which a list ~
-                       of changes should be printed.")))
+                       of changes should be printed.~@
+                       ~@
+                       True to print changes for all releases.~@
+                       ~@
+                       False to not print any changes.")))
   (:documentation
-   "Print the version of this program and some components."))
+   #.(format nil "Print the version of this program and some ~
+      components.~@
+      ~@
+      Optionally, also print changelog entries for a range of ~
+      releases.")))
 
 (service-provider:register-provider/class
  'command :version :class 'version)

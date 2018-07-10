a08b4f048f24cc2b497a3ceaea7938bd41dcd79e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-07-10 19:38:27 +0200

    Added retry utilities in src/analysis/util.lisp
    * src/analysis/util.lisp (*retry-count*):
      (with-retry-counter):
      (call-with-retry-restart):
      (with-retry-restart):
      (call-with-retries):
      (with-retries):

--- src/analysis/util.lisp
+++ src/analysis/util.lisp
@@ -6,6 +6,48 @@
 
 (cl:in-package #:jenkins.analysis)
 
+;;; Retrying
+
+(defun call-with-retry-restart (thunk report)
+  (tagbody
+   :start
+     (restart-case
+         (return-from call-with-retry-restart
+           (funcall thunk))
+       (retry ()
+         :report (lambda (stream) (funcall report stream))
+         (go :start)))))
+
+(defmacro with-retry-restart ((format-control &rest format-arguments) &body body)
+  `(call-with-retry-restart
+    (lambda () ,@body)
+    (lambda (stream)
+      (format stream ,format-control ,@format-arguments))))
+
+(defun call-with-retries (thunk condition-type limit)
+  (let ((retry-count 0))
+    (handler-bind
+        ((condition (lambda (condition)
+                      (cond
+                        ((not (typep condition condition-type)))
+                        ((>= retry-count limit)
+                         (error "~@<Giving up after ~D ~
+                                 retr~:@P. Original error: ~A~@:>"
+                                retry-count condition))
+                        (t
+                         (when-let ((restart (find-restart 'retry condition)))
+                           (incf retry-count)
+                           (log:warn "~@<Invoking retry restart ~A, ~
+                                      attempt ~D of ~D.~@:>"
+                                     restart retry-count limit)
+                           (invoke-restart restart)))))))
+      (funcall thunk))))
+
+(defmacro with-retries ((condition-type &key limit) &body body)
+  `(call-with-retries (lambda () ,@body) ',condition-type ,limit))
+
+;;;
+
 (defun default-temporary-directory (&key
                                     (base #P"/tmp/")
                                     (hint "build-generator"))

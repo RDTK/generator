208346b40d77b3fa74575fe5a271ee2822593991
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-09-25 11:04:24 +0200

    workspace root is project root directory

--- src/language-server/workspace.lisp
+++ src/language-server/workspace.lisp
@@ -15,7 +15,7 @@
 (defmethod load-templates ((container workspace))
   (let ((project::*templates* (make-hash-table :test #'equal))
         (project::*templates-lock* (bt:make-lock))
-        (pattern (merge-pathnames "../templates/toolkit/*.template"
+        (pattern (merge-pathnames "templates/toolkit/*.template"
                                   (lsp:root-directory container))))
     (log:error "Background-loading templates from ~A" pattern)
     (map nil #'project:load-template/yaml (directory pattern))
@@ -42,7 +42,7 @@
         (project::*projects* nil ; (make-hash-table :test #'equal)
                                            )
         (project::*projects-lock* (bt:make-lock))
-        (pattern "../projects/*.project" (lsp:root-directory container)))
+        (pattern "projects/*.project" (lsp:root-directory container)))
     (handler-bind ((error #'continue))
       (mappend (lambda (filename)
                  (with-simple-restart (continue "Skip")

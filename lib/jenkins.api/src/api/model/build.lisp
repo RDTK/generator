26efce843fdcc2dd482c5725b61e588ed578a3b8
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-11-21 18:55:37 +0100

    Added Subversion ChangeSet information in lib/jenkins.api/src/api/model/build.lisp
    * lib/jenkins.api/src/api/model/build.lisp
      (define-interface-implementations action): added implementation
      `subversion-change-log-set'
      (define-model-class build): changed XPath for actions slot to
      include the "changeSet" local name

--- lib/jenkins.api/src/api/model/build.lisp
+++ lib/jenkins.api/src/api/model/build.lisp
@@ -12,10 +12,18 @@
 ;;;
 ;;; * `action' interface and its implementations
 
-(define-interface-implementations (action :class-location (xloc:val "@*[local-name() = '_class']"))
+(define-interface-implementations
+    (action :class-location (xloc:val "@*[local-name() = '_class']"))
+
   ((git-build-data "hudson.plugins.git.util.BuildData")
    ((last-built-revision/sha1 :type  string
-                              :xpath "lastBuiltRevision/SHA1/text()"))))
+                              :xpath "lastBuiltRevision/SHA1/text()"))
+   (:name-slot last-built-revision/sha1))
+
+  ((subversion-change-log-set "hudson.scm.SubversionChangeLogSet")
+   ((revision :type  string
+              :xpath "revision/revision/text()"))
+   (:name-slot revision)))
 
 ;; TODO define-interface-implementations should do this
 (macrolet ((define-of-type-methods (interface
@@ -40,7 +48,10 @@
    (result     :type  keyword
                :xpath "result/text()")
    (actions    :type  action
-               :xpath ("action[@*[local-name() = '_class']]"
+               ;; Treat Subversion changeSet as action even though it
+               ;; isn't.
+               :xpath ("*[(local-name() = 'action' or local-name() = 'changeSet')
+                        and @*[local-name() = '_class']]"
                        :if-multiple-matches :all)))
   (:get-func (lambda (id) (build-config id))))
 

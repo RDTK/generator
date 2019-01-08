7b679f8030cf88d39399fc4c8fdf7d51f266940b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-01-08 12:37:34 +0100

    for command-release-distribution

--- src/commands/command-release-distribution.lisp
+++ src/commands/command-release-distribution.lisp
@@ -15,24 +15,32 @@
   (:default-initargs
    :output-file (missing-required-initarg 'release-distribution :output-file))
   (:documentation
-   "Write a distribution release into a new recipe file."))
+   #.(format nil "Write a distribution release into a new recipe file.~@
+      ~@
+      Revision information for project versions included in the ~
+      distribution is retrieved from the most recent builds of ~
+      corresponding Jenkins jobs.~@
+      ~@
+      Note: this only works reliably if a mode and overwrites matching ~
+      the values used to generate the jobs are supplied since things ~
+      like Jenkins job names depend on this information.")))
 
 (service-provider:register-provider/class
  'command :release-distribution :class 'release-distribution)
 
 (jenkins.project.commandline-options:define-option-mapping
     (*command-schema* "release-distribution")
-  (&rest                  "distributions"        "DISTRIBUTION-NAME"   t)
+  (&rest                  "distributions" "DISTRIBUTION-NAME"   t)
 
-  (("--mode" "-m")        "mode"                 "MODE")
-  (("--set" "-D")         "overwrites"           "VARIABLE-NAME=VALUE")
+  (("--mode" "-m")        "mode"          "MODE")
+  (("--set" "-D")         "overwrites"    "VARIABLE-NAME=VALUE")
 
-  (("--base-uri" "-b")    "base-uri"             "URI")
-  (("--username" "-u")    "username"             "LOGIN")
-  (("--password" "-p")    "password"             "PASSWORD")
-  (("--api-token" "-a")   "api-token"            "API-TOKEN")
+  (("--base-uri" "-b")    "base-uri"      "URI")
+  (("--username" "-u")    "username"      "LOGIN")
+  (("--password" "-p")    "password"      "PASSWORD")
+  (("--api-token" "-a")   "api-token"     "API-TOKEN")
 
-  (("--output-file" "-o") "output-file"          "OUTPUT-FILENAME"     t)) ; TODO make this positional?
+  (("--output-file" "-o") "output-file"   "OUTPUT-FILENAME"     t)) ; TODO make this positional?
 
 (defmethod command-execute ((command release-distribution))
   (let+ (((&accessors-r/o distributions mode overwrites output-file) command)
@@ -53,56 +61,28 @@
          (distribution (first distributions)))
     ; TODO (generate-check distributions)
 
-    (release-distribution distribution output-file)))
+    (release-distribution command distribution output-file)))
 
-(defun release-distribution (distribution output-file)
+(defun release-distribution (command distribution output-file)
   (let* ((location (jenkins.model.project::location-of (specification distribution)))
-         (text     (text.source-location:content
-                    (text.source-location:source location)))
+         (source   (text.source-location:source location))
          (jobs     (mappend (lambda (version)
                               (when-let ((job (find "main" (jobs version)
                                                     :test #'string= :key #'name))) ; TODO otherwise error?
                                 (list (cons version job))))
                             (versions distribution)))
-         (results  (inspect-jobs jenkins.api:*base-url*
-                                 jenkins.api:*username*
-                                 jenkins.api:*password*
-                                 jobs)))
-
+         (results  (as-phase (:inspect-jobs)
+                     (inspect-jobs jenkins.api:*base-url*
+                                   jenkins.api:*username*
+                                   jenkins.api:*password*
+                                   jobs))))
+    ;; Output description of the jobs' build statuses.
     (format-distribution-build-status *standard-output* results)
+    ;; Output new distribution file.
+    (with-output-to-file (stream output-file :if-exists :supersede)
+      (format-new-content stream command jobs results source))))
 
-    (let* ((new-text (copy-seq text))
-           (changes  '()))
-
-      (setf *jobs* jobs *results* results)
-
-      (loop :for (version . job) :in jobs
-            :for spec            = (jenkins.model:specification version)
-            :for result          = (gethash version results)
-            :for location        = (jenkins.model.project::location-of
-                                    (specification version))
-            :do (log:info spec version location)
-            :do (push (cons (text.source-location:index
-                             (text.source-location:start location))
-                            (lambda (text)
-                              (let ((start (text.source-location:index
-                                            (text.source-location:start location)))
-                                    (end   (text.source-location:index
-                                            (text.source-location:end location))))
-                                (log:info text start end)
-                                (concatenate 'string
-                                             (subseq text 0 start)
-                                             (format nil "- foo~%")
-                                             (subseq text end)))))
-                      changes))
-
-      (reduce (lambda (text change)
-                (funcall (cdr change) text))
-              (sort changes #'> :key #'car) :initial-value text)
-
-      (write-string-into-file text output-file :if-exists :supersede))))
-
-;;;
+;;; Inspecting jobs
 
 (defun inspect-jobs (base-url username password jobs)
   (let ((results (make-hash-table :test #'eq :synchronized t)))
@@ -184,3 +164,66 @@
                              :key (compose #'name #'parent #'specification #'car))
         :for (version . (job-id result data)) :in sorted
         :do (format-build-data stream version job-id result data)))
+
+;;; Output
+
+(defun compute-changes (jobs results source)
+  (loop :for (version . job)    :in jobs
+        :for spec               = (jenkins.model:specification version)
+        :for (nil nil revision) = (gethash version results)
+        :for location           = (jenkins.model.project::location-of
+                                   (specification version))
+        :if (not (eq (text.source-location:source location) source))
+          :do (log:warn "Skipping ~A" location)
+        :else
+          :collect (list (text.source-location:index
+                          (text.source-location:start location))
+                         (text.source-location:index
+                          (text.source-location:end location))
+                         (format nil "name: ~A~@
+                                        ~2@Tversion: ~A~@
+                                        ~2@Tparameters:~@
+                                        ~4@Tcommit: ~A"
+                                 (name (parent spec)) (name spec) revision))))
+
+(defun apply-changes (text changes)
+  (reduce (lambda+ (text (start end string))
+            (concatenate 'string
+                         (subseq text 0 start)
+                         string
+                         (subseq text end)))
+          (sort changes #'> :key #'first) :initial-value text))
+
+(defun format-header (stream command)
+  (format stream "# This file has been automatically generated:~@
+                  #~@
+                  #  User        ~A~@
+                  #  Host        ~A~@
+                  #  Date        ~A~@
+                  #  Generator   ~A~@
+                  #  Commandline ~{'~A'~^ ~}~@
+                  #~@
+                  #  Jenkins URL ~A~@
+                  #  Input file~P  ~{~A~^ ~}~@
+                  #  Mode        ~A~%~@[~
+                  #  Overwrites~@
+                  ~{#    ~(~36A~) → ~:A~%~}~
+                  ~]"
+          (sb-unix:uid-username (sb-posix:getuid))
+          (machine-instance)
+          (local-time:now)
+          (generator-version)
+          (uiop:command-line-arguments)
+
+          (base-uri command)
+          (length (distributions command)) (distributions command)
+          (mode command)
+          (alist-plist (overwrites command))))
+
+(defun format-new-content (stream command jobs results source)
+  (let* ((text    (text.source-location:content source))
+         (changes (compute-changes jobs results source))
+         (body    (apply-changes text changes)))
+    (format-header stream command)
+    (format stream "~2%")
+    (write-string body stream)))

9290e3d44a3365c5c14cb07d34b02f59f7f60ae6
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-01-18 12:42:18 +0100

    Better documentation printing in src/commandline-options/help.lisp
    * src/commandline-options/help.lisp (split-into-paragraphs): new
      function; semi-smartly split a string into paragraphs
      (print-documentation): use `split-into-paragraphs'

--- src/commandline-options/help.lisp
+++ src/commandline-options/help.lisp
@@ -63,18 +63,48 @@
 (defun designators< (left right &key positional-first?)
   (designator< (first left) (first right) :positional-first? positional-first?))
 
+(defun split-into-paragraphs (string)
+  (labels ((make-paragraph (characters)
+             (if (eql (first characters) #\Newline)
+                 (make-paragraph (rest characters))
+                 (coerce (nreverse characters) 'string))))
+    (loop :with paragraph =       '()
+          :for  previous  =       nil :then current
+          :for  current   :across string
+          :if (and (eql previous #\Newline) (eql current #\Newline))
+            :collect (make-paragraph paragraph) :into paragraphs
+            :and :do (setf paragraph '())
+          :else
+            :do (push current paragraph)
+          :finally (return (append paragraphs
+                                   (list (make-paragraph paragraph)))))))
+
+(assert (equal (split-into-paragraphs "foo bar
+baz fez
+
+whoop di
+do")
+               '("foo bar
+baz fez"
+                 "whoop di
+do")))
+
 (defun print-documentation (stream documentation &optional colon? at?)
   "`pprint-fill' the words in DOCUMENTATION onto STREAM."
   (declare (ignore colon? at?))
   (loop :with *print-escape* = nil
-     :with first? = t
-     :for paragraph :in (split-sequence:split-sequence
-                         #\Newline documentation
-                         :remove-empty-subseqs t)
-     :for words = (split-sequence:split-sequence #\Space paragraph)
-     :unless first? :do
-       (pprint-newline :mandatory stream)
-       (pprint-newline :mandatory stream)
-     :do
-       (setf first? nil)
-       (pprint-fill stream words nil)))
+        :with first? = t
+        :for paragraph :in (split-into-paragraphs documentation)
+        :unless first? :do
+           (pprint-newline :mandatory stream)
+           (pprint-newline :mandatory stream)
+        :do
+           (setf first? nil)
+           (loop :with lines  =   (split-sequence:split-sequence #\Newline paragraph)
+                 :with first? =   t
+                 :for  line   :in lines
+                 :for  words  =   (split-sequence:split-sequence #\Space line)
+                 :do (if first?
+                         (setf first? nil)
+                         (pprint-newline :mandatory stream))
+                     (pprint-fill stream words nil))))

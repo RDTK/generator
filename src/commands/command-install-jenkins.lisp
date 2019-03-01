d8bf50c13e78c169b0314902499119a200a3149d
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-03-01 19:04:33 +0100

    Added Jenkins user creation in src/commands/command-install-jenkins.lisp
    refs #5
    
    * src/commands/command-install-jenkins.lisp (install-jenkins::username):
      new slot; stores name of user that should be created
      (install-jenkins::email): new slot; stores email of user that should
      be created
      (install-jenkins::password): new slot; stores password of user that
      should be created
      (install-jenkins): mention user account setup in documentation
      string
      (define-option-mapping install-jenkins): added username, email and
      password commandline options
      (command-execute install-jenkins): execute step jenkins/create-user

--- src/commands/command-install-jenkins.lisp
+++ src/commands/command-install-jenkins.lisp
@@ -38,7 +38,26 @@
                          :reader   plugins
                          :initform *default-plugins*
                          :documentation
-                         "List of plugins to install."))
+                         "List of plugins to install.")
+   ;; User creation
+   (username             :initarg  :username
+                         :type     (or null string)
+                         :reader   username
+                         :initform nil
+                         :documentation
+                         "Username for the initial user account.")
+   (email                :initarg  :email
+                         :type     (or null string)
+                         :reader   email
+                         :initform nil
+                         :documentation
+                         "Email-address for the initial user account.")
+   (password             :initarg  :password
+                         :type     (or null string)
+                         :reader   password
+                         :initform nil
+                         :documentation
+                         "Password for the initial user account."))
   (:documentation
    #.(format nil "Install and configure a Jenkins CI server.~@
       ~@
@@ -48,22 +67,29 @@
         generator.~@
       ~@
       • Install Jenkins configuration files that make the instance ~
-        work well with the build-generator.")))
+        work well with the build-generator.~@
+      ~@
+      • Set up a user account.")))
 
 (service-provider:register-provider/class
  'command :install-jenkins :class 'install-jenkins)
 
 (jenkins.project.commandline-options:define-option-mapping
     (*command-schema* "install-jenkins")
-  (0                        "output-directory"     "DIRECTORY" t)
+  (0                        "output-directory"     "DIRECTORY"     t)
 
   ("--jenkins-download-url" "jenkins-download-url" "URL")
 
-  (("--plugin" "-p")        "plugins"              "PLUGIN"))
+  (("--plugin" "-p")        "plugins"              "PLUGIN")
+
+  (("--username" "-u")      "username"             "USENAME")
+  (("--email" "-e")         "email"                "EMAIL-ADDRESS")
+  (("--password" "-p")      "password"             "PASSWORD"))
 
 (defmethod command-execute ((command install-jenkins))
   (let+ (((&accessors-r/o output-directory
-                          jenkins-download-url plugins)
+                          jenkins-download-url plugins
+                          username email password)
           command))
     (as-phase (:install)
       (with-trivial-progress (:install/core)
@@ -81,4 +107,13 @@
       (jenkins.project.steps:execute
        (jenkins.project.steps:make-step :jenkins/install-config-files) nil
        :destination-directory output-directory
-       :config-files          jenkins.project.steps::*jenkins-config-files*))))
+       :config-files          jenkins.project.steps::*jenkins-config-files*)
+
+      (when (and username email password)
+        (jenkins.project.steps:execute
+         (jenkins.project.steps:make-step :jenkins/create-user) nil
+         :destination-directory output-directory
+         :config-file-template  jenkins.project.steps::*jenkins-user-config-file-template*
+         :username              username
+         :email                 email
+         :password              password)))))

9a4f14c0c3dd5d5feaa0ea1d9d4e94b317e38acb
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-11-26 12:28:34 +0100

    Added action interface in lib/jenkins.api/src/api/model/build.lisp
    Added the action interface for builds with implementations for
    old-style and new-style Git build information. Also shoehorned
    Subversion ChangeSet information into the same framework.
    
    * lib/jenkins.api/src/api/model/build.lisp
      (define-interface-implementations action): new interface definition
      for build actions
      (define-model-class build): added actions slot with XPath for actual
      as well as the "changeSet" local name

--- lib/jenkins.api/src/api/model/build.lisp
+++ lib/jenkins.api/src/api/model/build.lisp
@@ -6,13 +6,37 @@
 
 (cl:in-package #:jenkins.api)
 
+(define-interface-implementations
+    (action :class-location (xloc:val "@*[local-name() = '_class']"))
+
+  ((git-build-data "hudson.plugins.git.util.BuildData")
+   ((last-built-revision/sha1 :type  string
+                              :xpath "lastBuiltRevision/SHA1/text()"))
+   (:name-slot last-built-revision/sha1))
+
+  ((git-build-details "hudson.plugins.git.util.BuildDetails")
+   ((revision/sha1 :type  string
+                   :xpath "build/revision/SHA1/text()"))
+   (:name-slot revision/sha1))
+
+  ((subversion-change-log-set "hudson.scm.SubversionChangeLogSet")
+   ((revision :type  string
+              :xpath "revision/revision/text()"))
+   (:name-slot revision)))
+
 (define-model-class build ()
   ((building?  :type  boolean
                :xpath "building/text()")
    (slave-name :type  string
                :xpath "builtOn/text()")
    (result     :type  keyword
-               :xpath "result/text()"))
+               :xpath "result/text()")
+   (actions    :type  action
+               ;; Treat Subversion changeSet as action even though it
+               ;; isn't.
+               :xpath ("*[(local-name() = 'action' or local-name() = 'changeSet')
+                        and @*[local-name() = '_class']]"
+                       :if-multiple-matches :all)))
   (:get-func (lambda (id) (build-config id)))
   (:name-slot nil))
 

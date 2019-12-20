a09661f45f3f74f498bc6a3930461d26031a596b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-12-20 09:43:43 +0100

    Updated warnings-ng version in lib/jenkins.api/src/api/model/job.lisp
    * lib/jenkins.api/src/api/model/job.lisp
      (define-interface-implementation publisher): updated warnings-ng
      plugin version to 7.2.2; added clauses for minimum-severity XPath

--- lib/jenkins.api/src/api/model/job-publisher.lisp
+++ lib/jenkins.api/src/api/model/job-publisher.lisp
@@ -228,7 +228,7 @@
    (:name-slot nil))
 
   ((issues-recorder "io.jenkins.plugins.analysis.core.steps.IssuesRecorder"
-                    :plugin "warnings-ng@7.2.1")
+                    :plugin "warnings-ng@7.2.2")
    ((analysis-tools                  :type      analysis-tool
                                      :xpath     ("analysisTools/*"
                                                  :if-multiple-matches :all)
@@ -258,6 +258,7 @@
                                      :initform  0)
     (minimum-severity                :type      string
                                      :xpath     (:version
+                                                 ("warnings-ng@7.2.2" "minimumSeverity[@plugin='analysis-model-api@7.0.2']/name/text()")
                                                  ("warnings-ng@7.2.1" "minimumSeverity[@plugin='analysis-model-api@7.0.2']/name/text()")
                                                  ("warnings-ng@7.0.0" "minimumSeverity[@plugin='analysis-model-api@7.0.1']/name/text()")
                                                  ("warnings-ng@5.1.0" "minimumSeverity[@plugin='analysis-model-api@5.1.1']/name/text()")
